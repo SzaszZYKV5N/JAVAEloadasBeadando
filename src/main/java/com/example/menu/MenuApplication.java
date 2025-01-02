1b0d1a6931bfe8162ad85a270fa6ebfea8ee88cc
SzaszZYKV5N <szasz.andras002@gmail.com>
2025-01-02 15:07:25 +0100

    JavaFX menü létrehozás
    Alap menürendszer kialakítása, Controller elkészítése

---
+++ src/main/java/com/example/menu/MenuApplication.java
@@ -0,0 +1,24 @@
+package com.example.menu;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MenuApplication extends Application {
+public String oldal ="menu.fxml";
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MenuApplication.class.getResource(oldal));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 550);
+        stage.setTitle("JAVA elméleti beadandó feladat");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/com/example/menu/MenuController.java
@@ -0,0 +1,59 @@
+package com.example.menu;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
+public class MenuController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+    @FXML
+    AnchorPane rootPane;
+
+    @FXML
+    private void Ujablak(ActionEvent event) throws IOException {
+        AnchorPane pane = FXMLLoader.load(getClass().getResource("second.fxml"));
+        rootPane.getChildren().setAll(pane);
+    }
+    private void loadFXMLToRootPane(String fxmlFileName) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlFileName));
+            Parent newContent = loader.load();
+            rootPane.getChildren().setAll(newContent);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    @FXML
+    private void handleMenuAction(ActionEvent event) {
+        MenuItem source = (MenuItem) event.getSource();
+        String fxmlFileName = "";
+
+        switch (source.getText()) {
+            case "Olvas":
+                fxmlFileName = "olvas.fxml";
+                break;
+            case "Olvas2":
+                fxmlFileName = "olvas2.fxml";
+                break;
+            // További esetek hozzáadása szükség szerint
+            default:
+                System.out.println("Ismeretlen menüpont: " + source.getText());
+                return;
+        }
+
+        loadFXMLToRootPane(fxmlFileName);
+    }
+
+}
